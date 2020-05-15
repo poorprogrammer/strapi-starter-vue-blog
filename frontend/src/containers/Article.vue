9a1af0383302613a098fc4ab96479b297c64a75e
jane <jane@odd-e.comjane@odd-e.com>
2020-05-15 23:48:17 +0700

    Custom something

--- frontend/src/containers/Article.vue
+++ frontend/src/containers/Article.vue
@@ -12,35 +12,32 @@
 
     <div class="uk-section">
       <div class="uk-container uk-container-small">
-        <vue-markdown-it
-          v-if="article.content"
-          :source="article.content"
-          id="editor"
-        />
-        <p v-if="article.published_at">
-          {{ moment(article.published_at).format("MMM Do YY") }}
-        </p>
+        <!-- <vue-markdown-it v-if="article.content" :source="article.content" id="editor" /> -->
+        <div v-html="article.content"></div>xxx
+        <div v-html="article.video"></div>
+
+        <p v-if="article.published_at">{{ moment(article.published_at).format("MMM Do YY") }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-var moment = require("moment");
-import VueMarkdownIt from "vue-markdown-it";
-import gql from "graphql-tag";
+var moment = require('moment')
+// import VueMarkdownIt from 'vue-markdown-it'
+import gql from 'graphql-tag'
 
 export default {
   data() {
     return {
       article: {},
       moment: moment,
-      api_url: process.env.VUE_APP_STRAPI_API_URL || "http://localhost:1337",
+      api_url: process.env.VUE_APP_STRAPI_API_URL || 'http://localhost:1337',
       routeParam: this.$route.params.id
-    };
+    }
   },
   components: {
-    VueMarkdownIt
+    // VueMarkdownIt
   },
   apollo: {
     article: {
@@ -54,15 +51,16 @@ export default {
               url
             }
             published_at
+            video
           }
         }
       `,
       variables() {
         return {
           id: this.routeParam
-        };
+        }
       }
     }
   }
-};
+}
 </script>
